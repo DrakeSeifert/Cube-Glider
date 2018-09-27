8e0fab5c0a5ae335f86f63189595845ac0c9afea
DrakeSeifert <drakeseifert1@gmail.com>
2018-09-27 16:22:45 -0700

    Added a Menu and Credits screen

---
+++ Assets/Credits.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Credits : MonoBehaviour {
+
+    public void Quit()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+
+    public void GoToMenu()
+    {
+        //Menu Scene should be at location 0 in the build index
+        SceneManager.LoadScene(0);
+    }
+
+}
